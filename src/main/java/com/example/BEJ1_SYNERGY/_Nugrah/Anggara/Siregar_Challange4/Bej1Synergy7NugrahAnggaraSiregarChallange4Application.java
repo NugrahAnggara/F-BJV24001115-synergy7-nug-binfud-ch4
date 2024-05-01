7c2628601519dec6f5aee3cc860908fc9facea5c
unknown <nugrahanggarasiregar@mhs.unimed.ac.id>
2024-05-01 22:47:13 +0700

    initialize project rest API

---
+++ src/main/java/com/example/BEJ1_SYNERGY/_Nugrah/Anggara/Siregar_Challange4/Bej1Synergy7NugrahAnggaraSiregarChallange4Application.java
@@ -0,0 +1,13 @@
+package com.example.BEJ1_SYNERGY._Nugrah.Anggara.Siregar_Challange4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bej1Synergy7NugrahAnggaraSiregarChallange4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bej1Synergy7NugrahAnggaraSiregarChallange4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BEJ1_SYNERGY/_Nugrah/Anggara/Siregar_Challange4/Bej1Synergy7NugrahAnggaraSiregarChallange4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BEJ1_SYNERGY._Nugrah.Anggara.Siregar_Challange4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bej1Synergy7NugrahAnggaraSiregarChallange4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
